69bdcdf57b94a7ba5bb8b18aa9e65df735724e11
Hiléo Andersson <hileomsi@gmail.com>
2019-11-29 23:03:44 -0300

    Initial Commit

---
+++ uikit-my-diet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  uikit-my-diet
+//
+//  Created by Hiléo Andersson on 29/11/19.
+//  Copyright © 2019 Hiléo Andersson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
